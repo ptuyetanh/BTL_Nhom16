da9ea2a96f05a2bad3e3c8917b162b0c8a1a04ec
TuyếtAnh <phamtuyetanh2@gmail.com>
2021-12-19 16:22:42 +0700

    add login.php css img

---
+++ login.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twitter.Tham gia Twitter hôm nay.</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 img-login">
+                <i class="bi bi-twitter text-light"></i>
+            </div>
+            <div class="col-md-6 mt-5 sidebar main-login">
+                <i class="bi bi-twitter text-primary"></i>
+                <h1 class="t-1">Đang diễn ra ngay bây giờ</h1>
+                <h4 class="t-2">Tham gia Twitter hôm nay.</h4>
+                <div class="dk">
+                    <button type="button" class="btn btn-outline-primary btn-dangki">Đăng kí</button>
+                    <p>Khi đăng ký, bạn đã đồng ý với <a href="#">Điều khoản Dịch vụ</a> <br> và <a href="#">Chính sách
+                            Quyền
+                            riêng tư</a>, gồm cả <a href="#">Sử dụng Cookie</a>.</p>
+                </div>
+                <div class="login mt-4">
+                    <h6>Bạn đã có tài khoản?</h6>
+                    <button type="button" class="btn btn-outline-primary btn-login">Đăng nhập</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer -->
+    <footer class="container-fluid">
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link text-muted " href="#">Giới Thiệu</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Trung Tâm trợ giúp</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Điều khoản dịch vụ </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Chính sách riêng tư </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Chính sách cookie</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Thông tin quảng cáo</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Trạng Thái</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Nghề nghiệp</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Tài nguyên thương hiệu</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Quảng cáo</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Tiếp thị</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Twitter cho doanh nghiệp</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Nhà phát triển </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Danh mục</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Cài đặt</a>
+            </li>
+        </ul>
+            <div class="text-center text-muted footer2">
+                &copy;2021Twitter
+            </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
