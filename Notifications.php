fe305313712c1a3e9fb4cda42329f54a910c3119
nguyenthuhang2001 <1951060688@e.tlu.edu.vn>
2021-12-22 23:47:16 +0700

    add Notifications.php css

---
+++ Notifications.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Twitter.Tham gia Twitter hôm nay.</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+ <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 sidebar mt-auto">
+
+    </div>
+      <div class="col-md-6 main">
+        <div>
+          <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid ">
+              <a class="navbar-brand"><b>Thông báo</b></a>
+              <a href=""><i class="bi bi-gear text-dark"></i></a>
+            </div>
+          </nav>
+        </div>
+        <div>
+          <!-- Tabs navs -->
+          <ul class="nav nav-tabs nav-fill mb-3" id="ex1" role="tablist">
+            <li class="nav-item" role="presentation">
+              <a class="nav-link active" id="ex2-tab-1" data-mdb-toggle="tab" href="#ex2-tabs-1" role="tab"
+                aria-controls="ex2-tabs-1" aria-selected="true"><b>Tất cả</b></a>
+            </li>
+            <li class="nav-item" role="presentation">
+              <a class="nav-link" id="ex2-tab-2" data-mdb-toggle="tab" href="#ex2-tabs-2" role="tab"
+                aria-controls="ex2-tabs-2" aria-selected="false"><b>Đề cập</b></a>
+            </li>
+          </ul>
+          <!-- Tabs navs -->
+        </div>
+        <div>
+          <div class="container notifi">
+            <div class="row">
+              
+              <link rel="stylesheet" href="">
+              <div class="col-md-1">
+                <i class="bi2 bi-star-fill fs-3 "></i>
+              </div>
+              <div class="col-md-8">
+              <img src="./img/no-image.png" alt="" class="rounded-circle " width="40" height="40">
+                </i>
+                <br> <p>tên đăng nhập</p>
+                
+              </div>
+              <div class="col-md-1">
+                <i class="bi bi-three-dots "></i>
+
+              </div>
+            </div>
+            <div class="row">
+              <link rel="stylesheet" href="">
+              <div class="col-md-1">
+                <i class="bi2 bi-star-fill fs-3 "></i>
+              </div>
+              <div class="col-md-8">
+              <img src="./img/no-image.png" alt="" class="rounded-circle " width="40" height="40">
+                </i>
+                <br> <p>tên đăng nhập</p>
+                
+              </div>
+              <div class="col-md-1">
+                <i class="bi bi-three-dots "></i>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 search w-5 h-5">
+        
+    </div>
+    </div>
+  </div>
+
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
