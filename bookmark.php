7ad44996b25dcba80880078dfea8c4596ceb3903
lanhuong1234 <phamlanhuong946@gmail.com>
2021-12-25 20:51:08 +0700

    add bookmarks

---
+++ bookmark.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twitter</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 sidebar ">
+                shfgsg
+            </div>
+            <!--main-->
+            <div class="col-md-6 mainbookmarks border-start border-end ">
+                <div class="col-md-8">
+                    <div class="header_text">
+                        <strong>Dấu trang</strong><br>
+                    </div>
+                    <span>@Tên đăng nhập</span>
+                </div>
+                <div class="col-md-2">
+                    <img src="./img/no-image.png" alt="" class="rounded-circle ms-5" width="40" height="40">
+                </div>
+                <div class="col-md-10 ">
+                    <div class="post_header container-fluid row">
+                        <div class="col-md-10 post_header_left">
+                            <a class="Name " href=""><strong>Tên người dùng</strong></a>
+                            <span>@tên đăng nhập</span>
+                            <span>ngày</span>
+                        </div>
+                        <div class="col-md-2 post_header_right">
+                            <i class="bi bi-three-dots "></i>
+                        </div>
+                    </div>
+                    <div class="post_body ms-4">
+                        <div> post text..... </div>
+                    </div>
+                </div>
+            </div>
+            <!--search-->
+            <div class="col-md-3 search">
+                hdfg
+            </div>
+        </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
