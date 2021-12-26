b26763a829eeaedf23acab2fffc97767573d18b0
nguyenthuhang2001 <1951060688@e.tlu.edu.vn>
2021-12-26 23:33:31 +0700

    thêm notifi

--- Notifications.php
+++ Notifications.php
@@ -1,6 +1,23 @@
-<?php
-     include("template/sidebar.php");
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Twitter.Tham gia Twitter hôm nay.</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+ <link rel="stylesheet" href="css/style.css">
+</head>
+ 
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 sidebar mt-auto ">
+
+    </div>
       <div class="col-md-6 mainnotifi border-start border-end">
         <div>
           <nav class="navbar navbar-light bg-light">
@@ -28,10 +45,10 @@
           <div class="container notifi">
           <div class="row">
               <link rel="stylesheet" href="">
-              <div class="col-md-2">
+              <div class="col-md-1">
                 <i class="bi2 bi-star-fill fs-3 "></i>
               </div>
-              <div class="col-md-9">
+              <div class="col-md-10">
               <img src="./img/no-image.png" alt="" class="rounded-circle " width="40" height="40">
                 </i>
                 <br> <p>tên đăng nhập</p>
@@ -44,29 +61,39 @@
             </div>
             <div class="row">
               <link rel="stylesheet" href="">
-              <div class="col-md-2">
-                <i class="bi bi-heart-fill"></i>
+              <div class="col-md-1">
+                <i class="bitim bi-heart-fill"></i>
               </div>
-              <div class="col-md-9">
+              <div class="col-md-10">
               <img src="./img/no-image.png" alt="" class="rounded-circle " width="40" height="40">
                 </i>
                 <br> <p>tên đăng nhập</p>
                 
               </div>
-              <div class="col-md-1">
-                <i class="bi bi-three-dots "></i>
-
-              </div>
+             
             </div>
 
             <div class="row">
               
               <link rel="stylesheet" href="">
-              <div class="col-md-2">
+              <div class="col-md-1">
               <i class="bi bi-person-fill fs-3 user"></i>
               </div>
-              <div class="col-md-9">
+              <div class="col-md-10">
               <img src="./img/no-image.png" alt="" class="rounded-circle " width="40" height="40">
+                <br> <p>tên đăng nhập</p>
+                
+              </div>
+              
+            </div>
+            <div class="row">
+              <link rel="stylesheet" href="">
+              <div class="col-md-1">
+                <i class="bi2 bi-star-fill fs-3 "></i>
+              </div>
+              <div class="col-md-10">
+              <img src="./img/no-image.png" alt="" class="rounded-circle " width="40" height="40">
+                </i>
                 <br> <p>tên đăng nhập</p>
                 
               </div>
@@ -77,14 +104,36 @@
             </div>
             <div class="row">
               <link rel="stylesheet" href="">
-              <div class="col-md-2">
-                <i class="bi2 bi-star-fill fs-3 "></i>
+              <div class="col-md-1">
+                <img src="./img/no-image.png" alt="" class="rounded-circle " width="40" height="40">
+              </i>
               </div>
-              <div class="col-md-9">
-              <img src="./img/no-image.png" alt="" class="rounded-circle " width="40" height="40">
-                </i>
-                <br> <p>tên đăng nhập</p>
-                
+              <div class="col-md-10 ">
+              
+                <div class="post_header-notifi container-fluid row">
+                  
+                      <a class="Namenotifi " href=""><strong>Tên người dùng</strong></a>
+                      <span>@tênđăngnhap</span>
+                      <span>ngày</span>
+                 
+                 
+              </div>
+                <div class="post_footer">
+                  <ul class="nav nav-pills nav-fill mt-3 me-5">
+                      <li class="nav-item">
+                        <a class="fs-5" href=""><i class="bi bi-chat"></i></a>
+                      </li>
+                      <li class="nav-item ">
+                        <a class="fs-5" href="#"><i class="bi bi-arrow-left-right"></i></a>
+                      </li>
+                      <li class="nav-item">
+                        <a class=" fs-5" href="#"><i class="bi bi-heart"></i></a>
+                      </li>
+                      <li class="nav-item">
+                        <a class=" fs-5"><i class="bi bi-share"></i></a>
+                      </li>
+                    </ul>
+              </div>
               </div>
               <div class="col-md-1">
                 <i class="bi bi-three-dots "></i>
@@ -94,6 +143,17 @@
           </div>
         </div>
       </div>
-<?php
-     include("template/footer.php");
-?>
+      <div class="col-md-3 search w-5 h-5 ">
+        
+    </div>
+    </div>
+  </div>
+
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
