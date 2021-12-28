54a2e620814ab73bbb70fea55ca9383e5e587170
TuyếtAnh <phamtuyetanh2@gmail.com>
2021-12-29 02:27:26 +0700

    add send-mail.php PHpMailer, xử lí process-signup.php thông qua send-email kích hoạt tài khoản

--- process-signup.php
+++ process-signup.php
@@ -1,46 +1,90 @@
 <?php
-if(isset($_POST['btnsignup'])){
-    header("location:signup.php");
+if(isset($_POST['btnsignup']) && $_POST['txtemail'])//kiểm tra người dùng nhấp vào nút submit chưa và đã nhập email chưa
+{   // header("location:signup.php");
+        //coi dữ liệu là hợp lệ
+        $name = $_POST['txtname'];
+        $user = $_POST['txtuser'];
+        $email = $_POST['txtemail'];
+        $pass1 = $_POST['txtPass1'] ;
+        $pass2 = $_POST['txtPass2'] ;
+        // Bước 01: Kết nối Database Server
+        $conn = mysqli_connect('localhost','root','','twitter');
+        if(!$conn){
+            die("Kết nối thất bại. Vui lòng kiểm tra lại các thông tin máy chủ");
+        }
+        // Bước 02: Thực hiện truy vấn
+        $sql01 = "SELECT * FROM db_nguoidung WHERE email = '$email'  OR	tendangnhap='$user'";
+        // Ở đây còn có các vấn đề về tính hợp lệ dữ liệu nhập vào FORM
+        // Nghiêm trọng: lỗi SQL Injection
+
+        $result01 = mysqli_query($conn,$sql01);
+        $result01 = mysqli_query($conn,"SELECT * FROM db_nguoidung WHERE email='$email'");
+        if(mysqli_num_rows($result01) > 0){
+            $error = "Bạn nhập thông tin Email đã tồn tại";
+            header("location: signup.php?error=$error"); //Chuyển hướng, hiển thị thông báo lỗi
+        }else//Kiểm tra email chưa được dùng (mysqli_num_rows($result01) <= 0)
+        {
+            $token = md5($_POST['txtemail']).rand(10,9999);//sử dụng giải thuật md5 để sinh ra chuỗi ngẫu nhiên được băm
+            $pass_md5 = md5($pass1);
+            $pass_hash = password_hash($pass1,PASSWORD_DEFAULT );
+            //ra lệnh lưu vào CSDL
+            $sql02 = "INSERT INTO db_nguoidung (tendangnhap,email ,matkhau,tennguoidung,email_verification_link) VALUES ('$user','$email','$pass_hash','$name','$token')";
+            $result02 = mysqli_query($conn,$sql02);
+            //Sau khi lưu xong chúng ta cần gửi tới email đăng kí đường link tới website của chúng ta 
+            //yêu cầu người dùng kích hoạt ;biến link sẽ được gửi vào email
+            $link = "<a href='localhost/Twitter1/BTL_Nhom16/activation.php?key=".$email."&token=".$token."'>Nhấp vào đây để kích hoạt</a>";
+           include "send-mail.php";
+           if(sendEmailForAccountActive($email,$link)){
+               echo "vui lòng kiểm tra hộp thư của bạn để kích hoạt tài khoản";
+           }
+           else{
+               echo "Xin lỗi email chưa được gửi đi .Vui lòng kiểm tra lại thông tin đăng kí tài khoản";
+           }
+            //Quá trình gửi email
+            // require_once('phpmail/PHPMailerAutoload.php');
+            // $mail = new PHPMailer();
+            // $mail->CharSet =  "utf-8";
+            // $mail->IsSMTP();
+            // // enable SMTP authentication
+            // $mail->SMTPAuth = true;                  
+            // // GMAIL username
+            // $mail->Username = "your_email_id@gmail.com";
+            // // GMAIL password
+            // $mail->Password = "your_gmail_password";
+            // $mail->SMTPSecure = "ssl";  
+            // // sets GMAIL as the SMTP server
+            // $mail->Host = "smtp.gmail.com";
+            // // set the SMTP port for the GMAIL server
+            // $mail->Port = "465";
+            // $mail->From='your_gmail_id@gmail.com';
+            // $mail->FromName='your_name';
+            // $mail->AddAddress('reciever_email_id', 'reciever_name');
+            // $mail->Subject  =  'Reset Password';
+            // $mail->IsHTML(true);
+            // $mail->Body    = 'Click On This Link to Verify Email '.$link.'';
+            // if($mail->Send())
+            // {
+            //     echo "Check Your Email box and Click on the email verification link.";
+            // }
+            // else
+            // {
+            // echo "Mail Error - >".$mail->ErrorInfo;
+            // }
+        }
+        // else
+        // {
+        // echo "You have already registered with us. Check Your email box and verify email.";
+        // }
+        //  if ( $result02 == TRUE){
+        //     header("location: signin.php");
+        //  }
+        //  else{
+        //     $error = "Kiểm tra lại";
+        //     header("location: signup.php?error=$error"); //Chuyển hướng, hiển thị thông báo lỗi
+        //  }
+        
+
+        // Bước 03: Đóng kết nối
+        mysqli_close($conn);
 }
-//coi dữ liệu là hợp lệ
-$name = $_POST['txtname'];
-$user = $_POST['txtuser'];
-$email = $_POST['txtemail'];
-$pass1 = $_POST['txtPass1'] ;
-$pass2 = $_POST['txtPass2'] ;
-// Bước 01: Kết nối Database Server
-$conn = mysqli_connect('localhost','root','','twitter');
-if(!$conn){
-    die("Kết nối thất bại. Vui lòng kiểm tra lại các thông tin máy chủ");
-}
-// Bước 02: Thực hiện truy vấn
-$sql01 = "SELECT * FROM db_nguoidung WHERE email = '$email'  OR	tendangnhap='$user'";
-// Ở đây còn có các vấn đề về tính hợp lệ dữ liệu nhập vào FORM
-// Nghiêm trọng: lỗi SQL Injection
-
-$result01 = mysqli_query($conn,$sql01);
-if(mysqli_num_rows($result01) > 0){
-    $error = "Bạn nhập thông tin Email đã tồn tại";
-    header("location: signup.php?error=$error"); //Chuyển hướng, hiển thị thông báo lỗi
-    // CẤP THẺ LÀM VIỆC
-    
-}else{
-$pass_md5 = md5($pass1);
-$pass_hash = password_hash($pass1,PASSWORD_DEFAULT );
- $sql02 = "INSERT INTO db_nguoidung (tendangnhap,email ,matkhau,tennguoidung) VALUES ('$user','$email','$pass_hash','$name')";
- $result02 = mysqli_query($conn,$sql02);
-
- if ( $result02 == TRUE){
-    header("location: signin.php");
- }
- else{
-    $error = "Kiểm tra lại";
-    header("location: signup.php?error=$error"); //Chuyển hướng, hiển thị thông báo lỗi
- }
-  
-}
-
-// Bước 03: Đóng kết nối
-mysqli_close($conn);
-
 ?>
---
+++ send-mail.php
@@ -0,0 +1,63 @@
+<?php
+    $username = 'phamtuyetanh2@gmail.com';
+    $pass = 'yruispzthzenrjhx';
+    //khai bao 
+    
+    //Import PHPMailer classes into the global namespace
+    //These must be at the top of your script, not inside a function
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+    //2.su dung thư viện này để gửi Email(localhost) tới 1 tài khoản bất kì
+ function sendEmailForAccountActive($email){
+    //Create an instance; passing `true` enables exceptions
+    global $username;
+    global $pass;
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = $username;                     //SMTP username dong vai tro gui thu
+    $mail->Password   = $pass;                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;      
+    $mail->CharSet = 'UTF-8';                              //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('phamtuyetanh2@gmail.com','04_PhamTuyetAnh');
+    $mail->addAddress($email);     //Add a recipient    //Add a recipient
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = '[Twitter]Active your account';
+    $mail->Body    = 'Chào mừng bạn đến với twitter.Dể sử dụng tài khoản vui lòng nhấp <a href="http://localhost/Twitter/BTL_Nhom16/signin.php">Click here</a> để kích hoạt';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+   if($mail->send){
+       return true;
+   }
+   
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+   return false;
+ }
+?>
