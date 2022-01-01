c98d2af41a9be4261dbc1ed62d887a5e92f05043
nguyenthuhang2001 <1951060688@e.tlu.edu.vn>
2022-01-01 11:34:47 +0700

    xử lý mật khẩu thô với mật khẩu băm

--- process-signin.php
+++ process-signin.php
@@ -3,9 +3,9 @@
     session_start();
 
     //login.php TRUYỀN DỮ LIỆU SANG: NHẬN DỮ LIỆU TỪ login.php gửi sang
-    if(isset($_POST['btnSignIn'])){
-        $email = $_POST['txtEmail'];
-        $pass  = $_POST['txtPass'];
+    if(isset($_POST['btnSignIn'])&& isset($_POST['txtUser'])){
+        $email = $_POST['txtUser'];
+        $pass  = $_POST['txtPass'];//mật khẩu thô
         //Ở đây còn phải kiểm tra người dùng đã nhập chưa
      
         //Bước 01: Kết nối Database Server
@@ -14,19 +14,33 @@
             die("Kết nối thất bại. Vui lòng kiểm tra lại các thông tin máy chủ");
         }
         // Bước 02: Thực hiện truy vấn
-        $sql = "SELECT * FROM db_nguoidung WHERE email = '$email' AND matkhau='$pass'";
+        $sql = "SELECT * FROM db_nguoidung WHERE email = '$email' ";
         // Ở đây còn có các vấn đề về tính hợp lệ dữ liệu nhập vào FORM
         // Nghiêm trọng: lỗi SQL Injection
 
         $result = mysqli_query($conn,$sql);
+
         if(mysqli_num_rows($result) > 0){
-            //  CẤP THẺ LÀM VIỆC
+
+            $row = mysqli_fetch_assoc($result);
+            $pass_hash = $row['matkhau'];//lấy đc mk lưu trong sql
+
+            if(password_verify($pass, $pass_hash)){
+                //  CẤP THẺ LÀM VIỆC
             $_SESSION['isSigninOK'] = $email;
             header("location: index.php"); //Chuyển hướng về Trang quản trị
-         }else{
+
+            }
+            else{
+                $error = 'mật khẩu k chính xác';
+                header('location : signin.php');
+            }
+            
+         }
+            else{
              $error = "Bạn nhập thông tin Email hoặc mật khẩu chưa chính xác";
              header("location: signin.php?error=$error"); //Chuyển hướng, hiển thị thông báo lỗi
-        }
+                }
    
         // Bước 03: Đóng kết nối
         mysqli_close($conn);
--- signin.php
+++ signin.php
@@ -11,15 +11,15 @@
 </head>
 <body>
     <main class="form-signin mb-5">
-        <form>
+        <form class ="form-signin" action="process-signin.php" method = "post" >
             <i class="bi bi-twitter"></i>
             <h4>Đăng nhập vào Twitter</h4>
           <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <input type="email" id="inputEmail" name="txtUser" class="form-control" id="floatingInput" placeholder="name@example.com">
             <label for="floatingInput">Tên đăng nhập </label>
           </div>
           <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <input type="password"  id="inputPassword" name="txtPass"  class="form-control" id="floatingPassword" placeholder="Password">
             <label for="floatingPassword">Mật khẩu</label>
           </div>
       
@@ -32,9 +32,10 @@
                         echo "<h5 style='color:red'> {$_GET['error']} </h5>";
                     }
 
-            ?>
+                ?>
+               
           </div>
-          <button class="w-100 btn btn-lg btn-primary" type="submit">Đăng nhập</button>
+          <button class="w-100 btn btn-lg btn-primary" type="submit" name = "btnSignIn">Đăng nhập</button>
           <p class="mt-5 mb-3 text-muted footer-copy">&copy; 2021</p>
         </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
