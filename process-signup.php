6bdb17d96ddace3ba34b8c9ee418d8787c6849b9
TuyếtAnh <phamtuyetanh2@gmail.com>
2022-01-15 23:09:56 +0700

    fix lỗi

--- process-signup.php
+++ process-signup.php
@@ -1,12 +1,4 @@
 <?php
-    // trước khi cho người dùng xâm nhập vào bên trong
-    // phải kiểm tra Thẻ làm việc
-    session_start();
-    if(!isset($_SESSION['isSigninOK'])){
-        header("location:signin.php");
-    }
-?>
-<?php
 if(isset($_POST['btnsignup']) && $_POST['txtemail'])//kiểm tra người dùng nhấp vào nút submit chưa và đã nhập email chưa
 {   // header("location:signup.php");
         //coi dữ liệu là hợp lệ
