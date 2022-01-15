86db33442366c0fa210dc82772cb7ec671e9580a
TuyếtAnh <phamtuyetanh2@gmail.com>
2022-01-15 22:37:38 +0700

    fix lỗi

--- check-signup.php
+++ check-signup.php
@@ -1,11 +1,4 @@
-<?php
-    // trước khi cho người dùng xâm nhập vào bên trong
-    // phải kiểm tra Thẻ làm việc
-    session_start();
-    if(!isset($_SESSION['isSigninOK'])){
-        header("location:signin.php");
-    }
-?>
+
 <?php
 if($_POST['email'])//kiểm tra người dùng nhấp vào nút submit chưa và đã nhập email chưa
 {   // header("location:signup.php");
